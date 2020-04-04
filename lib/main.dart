26ca898cf923ede937a4e8344399da5dc5692396
Padam Ghimire <padamghimire75@gmail.com>
2020-04-04 22:55:07 +0545

    Api intrigrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 main(){
   runApp(
@@ -16,9 +19,51 @@ class Covid extends StatefulWidget {
 }
 
 class _CovidState extends State<Covid> {
+
+
+bool loading = true;
+List lstCountry;
+
+Future <String> _getWorldData() async{
+
+  var response = await http.get('https://brp.com.np/covid/country.php');
+  var getData = json.decode(response.body);
+
+  if(this.mounted){
+
+
+    setState(() {
+      loading = false;
+      lstCountry= [getData];
+    });
+  }
+}
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: (){
+
+          })
+        ],
+        title:Text('Covid-2019'),
+      ),
+     body: ListView(
+       padding:EdgeInsets.all(5),
+       children:<Widget>[
+         loading ? Center(child: CircularProgressIndicator()) :
+         ListView.builder(
+           shrinkWrap: true,
+           itemCount: lstCountry==null ? 0 :lstCountry.length,
+           physics: NeverScrollableScrollPhysics(),
+           itemBuilder:(context,index){
+             return listItem();
+           }
+         )
+       ]
+     ),
       
     );
   }
